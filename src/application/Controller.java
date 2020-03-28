ddd038f9f7323ecd61433f9999910f48ee69cbe3
Earl Cooke <ecooke@sfu.ca>
2020-03-28 14:30:08 -0700

    Remove assignment 2 code

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -87,40 +87,6 @@ public class Controller {
 		for (int m = height/2-2; m >=0; m--) {
 			freq[m] = freq[m+1] * Math.pow(2, -1.0/12.0); 
 		}
-		
-		volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-				@Override
-				public void changed(ObservableValue<? extends Number> observableVal, Number oldVal, Number newVal) {
-					// TODO Auto-generated method stub
-					Mixer.Info [] mixers = AudioSystem.getMixerInfo();  
-					for (Mixer.Info mixerInfo : mixers){
-					    Mixer mixer = AudioSystem.getMixer(mixerInfo);
-					    Line.Info [] lineInfos = mixer.getTargetLineInfo();
-					    for (Line.Info lineInfo : lineInfos){
-					        Line line = null;  
-					        boolean opened = true;  
-					        try {
-					            line = mixer.getLine(lineInfo);  
-					            opened = line.isOpen() || line instanceof Clip;
-					            if (!opened)    
-					                line.open();
-					            FloatControl volCtrl = (FloatControl)line.getControl(FloatControl.Type.VOLUME);  
-					            volCtrl.setValue(newVal.floatValue() / 100);
-					        }  
-					        catch (LineUnavailableException e) {  
-					            e.printStackTrace();  
-					        }  
-					        catch (IllegalArgumentException iaEx) {  
-					        }  
-					        finally {  
-					            if (line != null && !opened) 
-					                line.close();
-					        }  
-					    }
-					}
-				}
-    	});
-		volumeSlider.setValue(100);
 	}
 	
 	@FXML
@@ -148,104 +114,11 @@ public class Controller {
 			createFrameGrabber();
 		}
 	} 
-	
-	protected void play() throws LineUnavailableException {
-		// This method "plays" the image opened by the user
-		// You should modify the logic so that it plays a video rather than an image
-		if (image != null) {
-			// convert the image from RGB to grayscale
-			Mat grayImage = new Mat();
-			Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
 			
-			// resize the image
-			Mat resizedImage = new Mat();
-			Imgproc.resize(grayImage, resizedImage, new Size(width, height));
-			
-			// quantization
-			double[][] roundedImage = new double[resizedImage.rows()][resizedImage.cols()];
-			for (int row = 0; row < resizedImage.rows(); row++) {
-				for (int col = 0; col < resizedImage.cols(); col++) {
-					roundedImage[row][col] = (double)Math.floor(resizedImage.get(row, col)[0]/numberOfQuantizionLevels) / numberOfQuantizionLevels;
-				}
-			}
-			
-			// I used an AudioFormat object and a SourceDataLine object to perform audio output. Feel free to try other options
-	        AudioFormat audioFormat = new AudioFormat(sampleRate, sampleSizeInBits, numberOfChannels, true, true);
-            SourceDataLine sourceDataLine = AudioSystem.getSourceDataLine(audioFormat);
-            sourceDataLine.open(audioFormat, sampleRate);
-            sourceDataLine.start();
-            
-            for (int col = 0; col < width; col++) {
-            	byte[] audioBuffer = new byte[numberOfSamplesPerColumn];
-            	for (int t = 1; t <= numberOfSamplesPerColumn; t++) {
-            		double signal = 0;
-                	for (int row = 0; row < height; row++) {
-                		int m = height - row - 1; // Be sure you understand why it is height rather width, and why we subtract 1 
-                		int time = t + col * numberOfSamplesPerColumn;
-                		double ss = Math.sin(2 * Math.PI * freq[m] * (double)time/sampleRate);
-                		signal += roundedImage[row][col] * ss;
-                	}
-                	double normalizedSignal = signal / height; // signal: [-height, height];  normalizedSignal: [-1, 1]
-                	audioBuffer[t-1] = (byte) (normalizedSignal*0x7F); // Be sure you understand what the weird number 0x7F is for
-            	}
-            	sourceDataLine.write(audioBuffer, 0, numberOfSamplesPerColumn);
-            }
-            sourceDataLine.drain();
-            sourceDataLine.close();
-		} else {
-			// What should you do here?
-		}
-	} 
-	
-	/**
-	 * Plays a click noise
-	 * Adapted from https://stackoverflow.com/questions/2416935/how-to-play-wav-files-with-java
-	 * @throws LineUnavailableException
-	 * @throws IOException 
-	 * @throws UnsupportedAudioFileException 
-	 */
-	public static void playClick() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
-		// play 2click2furious wav file
-		String strFilename = "resources/2click2furious.wav";
-		File soundFile;
-		AudioInputStream audioStream;
-	    AudioFormat audioFormat;
-	    SourceDataLine sourceLine;
-	    
-	    soundFile = new File(strFilename);
-
-        audioStream = AudioSystem.getAudioInputStream(soundFile);
-        audioFormat = audioStream.getFormat();
-
-        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-
-        sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-        sourceLine.open(audioFormat);
-        sourceLine.start();
-
-        int nBytesRead = 0;
-        byte[] abData = new byte[BUFFER_SIZE];
-        while (nBytesRead != -1) {
-            try {
-                nBytesRead = audioStream.read(abData, 0, abData.length);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (nBytesRead >= 0) {
-                @SuppressWarnings("unused")
-                int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
-            }
-        }
-
-        sourceLine.drain();
-        sourceLine.close();
-	} 
-	
 	protected void createFrameGrabber() throws InterruptedException { 
 		if (capture != null && capture.isOpened()) { // the video must be open 
 			double framePerSecond = capture.get(Videoio.CAP_PROP_FPS); // create a runnable to fetch new frames periodically 
 			Runnable frameGrabber = new Runnable() {
-				int counter = 0;
 				@Override 
 				public void run() { 
 					Mat frame = new Mat(); 
@@ -253,12 +126,6 @@ public class Controller {
 						Image im = Utilities.mat2Image(frame);
 						Utilities.onFXThread(imageView.imageProperty(), im); 
 						image = frame;
-						try {
-							play();
-						} catch (LineUnavailableException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
 						double currentFrameNumber = capture.get(Videoio.CAP_PROP_POS_FRAMES); 
 						double totalFrameCount = capture.get(Videoio.CAP_PROP_FRAME_COUNT);
 						slider.setValue(currentFrameNumber / totalFrameCount * (slider.getMax() - slider.getMin())); 
@@ -266,20 +133,9 @@ public class Controller {
 							capture.release();
 							slider.setValue(0);
 						}
-						counter++;
 					} else { // reach the end of the video
 							capture.set(Videoio.CAP_PROP_POS_FRAMES, 0); 
 					} 
-					if (counter == 2) {
-						// play click noise
-						try {
-							playClick();
-						} catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						counter = 0;
-					}
 				} 
 			}; 
 			// terminate the timer if it is running 
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,11 @@
 package application;
 
-
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
-import javax.sound.sampled.Line;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.Mixer;
-
 import org.opencv.core.Core;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -33,41 +23,10 @@ public class Main extends Application {
 			Scene scene = new Scene(root, 600, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Blind Helper");
+			primaryStage.setTitle("CMPT365 - Final Project");
 			primaryStage.show();
 			primaryStage.setWidth(740);
 			primaryStage.setHeight(558);
-			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-		          public void handle(WindowEvent we) {
-		        	  // same code for handling volume control in Controller
-		              Mixer.Info [] mixers = AudioSystem.getMixerInfo();  
-						for (Mixer.Info mixerInfo : mixers){
-						    Mixer mixer = AudioSystem.getMixer(mixerInfo);
-						    Line.Info [] lineInfos = mixer.getTargetLineInfo();
-						    for (Line.Info lineInfo : lineInfos){
-						        Line line = null;  
-						        boolean opened = true;  
-						        try {
-						            line = mixer.getLine(lineInfo);  
-						            opened = line.isOpen() || line instanceof Clip;
-						            if (!opened)    
-						                line.open();
-						            FloatControl volCtrl = (FloatControl)line.getControl(FloatControl.Type.VOLUME);  
-						            volCtrl.setValue(0.0f);
-						        }  
-						        catch (LineUnavailableException e) {  
-						            e.printStackTrace();  
-						        }  
-						        catch (IllegalArgumentException iaEx) {  
-						        }  
-						        finally {  
-						            if (line != null && !opened) 
-						                line.close();
-						        }  
-						    }
-						}
-		          }
-		      });
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
